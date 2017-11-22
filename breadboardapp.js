dd4c192a3251b08497ac01f1b0bec3f21d3533bb
lazukr <peterh10201@gmail.com>
2017-11-22 01:48:57 +0100

    testing grid displaying

--- breadboardapp.js
+++ breadboardapp.js
@@ -8,24 +8,57 @@
  */ 
 
 const BEIGE = "#F5D3B3";
-
+const RED = "#FF0000";
 
 const WIDTH = 130;
 const HEIGHT = 35;
 const SQUARE_SIZE = 10;
+const TERMINAL_WIDTH = 63;
+const TERMINAL_HEIGHT = 5;
+const BUS_WIDTH = 40;
+const BUS_HEIGHT = 1;
 
+const upperTerminalArray = [];
+const lowerTerminalArray = [];
 
 function main() {
 
     const stage = new createjs.Stage("canvas");
+    initBoard();
     renderBoard(stage);
     stage.update();
 
 }
 
+function initBoard() {
+    
+    let i;
+
+    for (i = 0; i < TERMINAL_WIDTH; i++) {
+        upperTerminalArray.push(createNode(TERMINAL_HEIGHT, (i+1)*2));
+        lowerTerminalArray.push(createNode(TERMINAL_HEIGHT, (i+1)*2));
+    }
+}
+
+function createNode(slots, pos) {
+
+    const strip = [];
+    let i;
+
+    for (i = 0; i < slots; i++) {
+        strip.push((i+1)*2);
+    }
+
+    return {
+        pos: pos,
+        strip: strip
+    };
+}
+
+
 function renderBoard(stage) {
     
-    var rect = coordToGrid({
+    const rect = coordToGrid({
         x: 0,
         y: 0,
         w: WIDTH,
@@ -33,12 +66,20 @@ function renderBoard(stage) {
     });
 
     drawRect(stage, rect.x, rect.y, rect.w, rect.h, BEIGE);
-
+    upperTerminalArray.forEach(value => {
+        value.strip.forEach(pos => {
+            const square = coordToGrid({
+                x: value.pos,
+                y: pos
+            });
+            drawSquare(stage, square.x, square.y, RED); 
+        });
+    });
 }
 
 function drawSquare(stage, x, y, color) {
 
-    var square = new createjs.Shape();
+    const square = new createjs.Shape();
     square.graphics.beginFill(color).drawRect(x, y, SQUARE_SIZE, SQUARE_SIZE);
     stage.addChild(square);
 }
