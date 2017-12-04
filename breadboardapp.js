45d89f8b3af7e95ef31f66a3c93024b0a2f8fbb2
lazukr <peterh10201@gmail.com>
2017-12-04 22:07:15 +0100

    refactor for a more ecs format

--- breadboardapp.js
+++ breadboardapp.js
@@ -8,390 +8,414 @@
  */ 
 
 
-// colors
-const BEIGE = "#f5d3b3";
-const RED = "#f7786b";
-const BLUE = "#92a8d1";
-const GREEN = "#b1cbbb";
-const DARK_GREY = "#808080";
-const ORANGE = "#ff8c00";
+// breadboard colors
+const BREADBOARD_COLOUR = "#f5d3b3";
+const PIN_RED = "#f7786b";
+const PIN_BLUE = "#92a8d1";
+const PIN_GREEN = "#b1cbbb";
 
-// board dimensions
-const WIDTH = 132;
-const HEIGHT = 42;
-const BOARD_POS = {
-    x: 0,
-    y: 0
+// wire colors
+const RED = "#ff0000";
+const BLUE = "#0000ff";
+const LIME = "#00ff00";
+const YELLOW = "#ffff00";
+const MAGENTA = "#ff00ff";
+const CYAN = "#00ffff";
+const BLACK = "#000000";
+const WHITE = "#ffffff";
+const SILVER = "#c0c0c0";
+const GREY = "#808080";
+const MAROON = "#800000";
+const OLIVE = "#808000";
+const GREEN = "#008000";
+const PURPLE = "#800080";
+const TEAL = "#008080";
+const NAVY = "#000080";
+
+class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.serialized = `(${this.x},${this.y})`;
+  }
+
+  plus(position) {
+    return new Position(this.x + position.x, this.y + position.y);
+  }
+
+  times(value) {
+    return new Position(this.x * value, this.y * value);
+  }
+};
+
+class Size {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 };
 
 // number of pixels per grid
 const SQUARE_SIZE = 10;
+const INNER_SQUARE_SCALE  = 0.6;
+const INNER_SQUARE_OFFSET = new Position((1 - INNER_SQUARE_SCALE) / 2, (1-INNER_SQUARE_SCALE) / 2);
+const UNIT_SQUARE = new Size(1, 1);
+const INNER_UNIT_SQUARE = new Size(INNER_SQUARE_SCALE, INNER_SQUARE_SCALE);
+
+// board dimensions
+const BOARD_SIZE = new Size(132, 42);
+const BOARD_POSITION = new Position(0, 0);
 
 // terminal
-const TERMINAL_WIDTH = 63;
-const TERMINAL_HEIGHT = 5;
-const TERMINAL_OFFSET = {
-    x: 0,
-    y: 13
-};
-const TERMINAL_POS = {
-    x: 3,
-    y: 10
-};
-
+const TERMINAL_SIZE = new Size(63, 5);
+const TERMINAL_POSITION = new Position(3, 10);
+const TERMINAL_OFFSET = new Position(0, 13);
 
 // bus
-const BUS_WIDTH = 10;
-const BUS_HEIGHT = 5;
-const BUS_PIN_OFFSET = 12;
-const BUS_OFFSETS = {
-    red: {
-        x: 0,
-        y: 35
-    },
-    blue: {
-        x: 0,
-        y: 31
-    }
-};
+const BUS_SIZE = new Size(10, 5);
+const BUS_PIN_OFFSET = new Position(12, 0);
+const BUS_RED_POSITION = new Position(7, 3);
+const BUS_RED_OFFSET = new Position(0, 35);
+const BUS_BLUE_POSITION = new Position(7, 5);
+const BUS_BLUE_OFFSET = new Position(0, 31);
 
-const BUS_POS = {
-    red: {
-        x: 7,
-        y: 3
-    },
+const BUS_STRIP_SIZE = new Size(121, 1);
+const BUS_RED_STRIP_POS = new Position(5, 1);
+const BUS_BLUE_STRIP_POS = new Position(5, 7);
+const BUS_RED_STRIP_OFFSET = new Position(0, 39);
+const BUS_BLUE_STRIP_OFFSET = new Position(0, 27);
 
-    blue: {
-        x: 7,
-        y: 5
-    }
-}
-
-const BUS_STRIPS_SIZE = {
-    w: 121,
-    h: 1
-};
-const BUS_STRIPS_POS = {
-    red: [{x: 5, y: 1}, {x: 5, y: 40}],
-    blue: [{x: 5, y:7}, {x: 5, y:34}]
-};
-
+const BOARD_ID = "BOARD_ID";
 const PIN_ID = "PIN";
+const PIN_GRID = "PIN_GRID";
 
+class HashMap {
+  constructor() {
+    this.map = {};
+  }
+
+  has(key) {
+    return key in this.map;
+  }
+
+  get(key) {
+    return this.map[key];
+  }
+
+  set(key, value) {
+    this.map[key] = value;
+  }
+};
+
+class Display {
+  constructor(config) {
+    this.name = config.name;
+    this.position = config.position;
+    this.size = config.size;
+    this.colour = config.colour;
+    this.serialized = `${this.name}(${this.position.serialized})`;
+  }
+
+  toString() {
+    return this.serialized;
+  }
+};
+
+/*
+class Wire {
+  constructor(config) {
+    this.stage = config.stage;
+    this.color = config.color;
+    this.x = config.x;
+    this.y = config.y;
+    this.pins = [];
+    this.pins.push(config.startingPin);
+    this._initWire();
+  }
+
+  _initWire() {
+
+    this.display = new createjs.Shape().set({
+      x: this.x,
+      y: this.y,
+      mouseEnabled: false,
+      graphics: new createjs.Graphics().beginStroke(this.color).drawCircle(0, 0, 30)
+    });
+    this.stage.addChild(this.display);
+    this.mouseMoveEvent = this.stage.on("stagemousemove", this.drawLine, this);
+    this.mouseUpEvent = this.stage.on("stagemouseup", this.endDraw, this);
+  }
+
+  drawLine(e, data) {
+
+    let x, y;
+
+    if (data) { 
+      x = data.x;
+      y = data.y;
+    } else {
+      x = e.localX;
+      y = e.localY;
+    }
+
+    this.display.graphics.clear()
+      .setStrokeStyle(INNER_SQUARE_SIZE)
+      .beginStroke(this.color)
+      .moveTo(0,0)
+      .lineTo(x - this.display.x, y - this.display.y);
+  }
+
+  endDraw(e) {
+    let element;
+    let elements = this.stage.getObjectsUnderPoint(this.stage.mouseX, this.stage.mouseY);
+
+    for (let i = 0; i < elements.length; i++) {
+      if ((elements[i].name === PIN_ID) && (elements[i].pinObject.component == null)) {
+        element = elements[i];
+        break;               
+      }
+    }
+
+    if (element != null) {
+      this.drawLine(null, {
+        x: element.x + SQUARE_SIZE / 2,
+        y: element.y + SQUARE_SIZE / 2
+      });
+      this.pins.push(element.pinObject);
+      this.pins.forEach(pin => {
+        pin.component = this;
+      });
+    } else {
+      this.stage.removeChild(this.display);
+      this.pins = [];
+    }
+    this.stage.off("stagemousemove", this.mouseMoveEvent);
+    this.stage.off("stagemouseup", this.mouseUpEvent);
+  }
+}
+*/
+class Pin {
+  constructor(config) {
+    this.name = PIN_ID;
+    this.position = config.position;
+    this.colour = config.colour;
+    this._initPin();
+  }
+
+  _initPin() {
+    this.array = new Array();
+    this.array.push(new Display({
+      name: this.name,
+      position: this.position,
+      size: UNIT_SQUARE,
+      colour: this.colour,
+    }));
+    
+    this.array.push(new Display({
+      name: this.name,
+      size: INNER_UNIT_SQUARE,
+      position: this.position.plus(INNER_SQUARE_OFFSET),
+      colour: GREY,
+    }));
+  }
+
+  *[Symbol.iterator]() {
+    for (let i = 0; i < this.array.length; i++) {
+      yield this.array[i];
+    }
+  }
+};
+
+class PinArray {
+  constructor(config) {
+    if (new.target === PinArray) {
+      throw TypeError("You cannot call an abstract class");
+    }
+    this.size = config.size;
+    this.position = config.position;
+    this.colour = config.colour;
+    this._initArray();
+  }
+
+  _initArray() {
+    const width = this.size.width;
+    const height = this.size.height;
+
+    this.array = new Array(width);
+    for (let i = 0; i < width; i++) {
+      this.array[i] = new Array(height);
+      for (let j = 0; j < height; j++) {
+        this.array[i][j] = new Pin({
+          colour: this.colour,
+          position: this.position.plus(this._setPosition(i, j)),
+        });
+      }
+    }
+  }
+
+  _setPosition(i, j) {
+    return new Position(this.x + i, this.y + j);
+  }
+
+  *[Symbol.iterator]() {
+    for (let i = 0; i < this.array.length; i++) {
+      for (let j = 0; j < this.array[i].length; j++) {
+        yield this.array[i][j];
+      }
+    }
+  }
+}; 
+
+class Terminal extends PinArray {
+  constructor(config) {
+    super(config);
+  }
+
+  _setPosition(i, j) {
+    return new Position(i*2, j*2); 
+  }
+};
+
+class Bus extends PinArray {
+  constructor(config) {
+    super(config);
+  } 
+
+  _setPosition(i, j) {
+    return BUS_PIN_OFFSET.times(i).plus(new Position(j*2, 0));
+  }
+};
+
+class Board {
+  constructor(config) {
+    this.position = config.position;
+    this.size = config.size;
+    this.colour = config.colour;
+    this._initAllPins();
+  }
+
+  _initAllPins() {
+    this.array = new Array();
+    this.array.push(new Display({
+      name: BOARD_ID,
+      position: this.position,
+      size: this.size,
+      colour: this.colour,
+    }));
+
+    for (let i = 0; i < 2; i++) {
+      this.array.push(new Terminal({
+        size: TERMINAL_SIZE,
+        position: TERMINAL_POSITION.plus(TERMINAL_OFFSET.times(i)),
+        colour: PIN_GREEN,
+      }));
+
+      this.array.push(new Bus({
+        size: BUS_SIZE,
+        position: BUS_RED_POSITION.plus(BUS_RED_OFFSET.times(i)),
+        colour: PIN_RED,
+      }));
+
+      this.array.push(new Bus({
+        size: BUS_SIZE,
+        position: BUS_BLUE_POSITION.plus(BUS_BLUE_OFFSET.times(i)),
+        colour: PIN_BLUE,
+      }));
+
+      this.array.push(new Display({
+        size: BUS_STRIP_SIZE,
+        position: BUS_RED_STRIP_POS.plus(BUS_RED_STRIP_OFFSET.times(i)),
+        colour: PIN_RED,
+      }));
+
+      this.array.push(new Display({
+        size: BUS_STRIP_SIZE,
+        position: BUS_BLUE_STRIP_POS.plus(BUS_BLUE_STRIP_OFFSET.times(i)),
+        colour: PIN_BLUE,
+      }));
+    }
+  }
+
+  *[Symbol.iterator]() {
+    for (let i = 0; i < this.array.length; i++) {
+      yield this.array[i];
+    }
+  }
+};
+
+class View {
+  constructor(config) {
+    this.stage = new createjs.Stage('canvas');
+    this.gridSize = config.gridSize;
+  }
+
+  _drawRect(position, size, colour) {
+    const rect = new createjs.Shape();
+    rect.graphics
+      .beginFill(colour)
+      .drawRect(position.x, position.y, size.width, size.height);
+    this.stage.addChild(rect);
+  }
+
+  _toGridPosition(position) {
+    return new Position(position.x*this.gridSize, position.y*this.gridSize);
+  }
+
+  _toGridSize(size) {
+    return new Size(size.width*this.gridSize, size.height*this.gridSize);
+  }
+
+  _renderDisplayable(display) {
+    const actualSize = this._toGridSize(display.size);
+    const actualPosition = this._toGridPosition(display.position);
+    this._drawRect(actualPosition, actualSize, display.colour);
+  }
+
+  _renderDisplayCollection(displayCollection) {
+    for (let display of displayCollection) {
+      this.render(display);
+    }
+  }
+
+  render(displayObject) {
+    if (displayObject instanceof Display) {
+      this._renderDisplayable(displayObject);
+    }
+    
+    if ("array" in displayObject) {
+      this._renderDisplayCollection(displayObject);
+    }
+  }
+
+  update() {
+    this.stage.update();
+  }
+}; 
+
+class WebApp {
+  constructor(config) {
+    this.clear(); 
+  }
+  
+  clear() {
+    this.view = new View({
+      gridSize: SQUARE_SIZE, 
+    });
+    this.board = new Board({
+      position: BOARD_POSITION,
+      size: BOARD_SIZE,
+      colour: BREADBOARD_COLOUR, 
+    });
+  };
+
+  update() {
+    this.view.render(this.board);
+    this.view.update();
+  }
+};
 
 function main() {
 
-    const board = new Board({
-        color: BEIGE,
-        width: WIDTH,
-        height: HEIGHT
-    });
-    board.render();
-
-}
-
-function test() {
-    console.log("hi");
-
-}
-
-class Wire {
-    constructor(config) {
-        this.stage = config.stage;
-        this.color = config.color;
-        this.x = config.x;
-        this.y = config.y;
-        this.pins = [];
-        this.pins.push(config.startingPin);
-        this.display = null;
-        this.mouseMoveEvent = null;
-        this.mouseUpEvent = null;
-        this.initWire();
-    }
-
-    initWire() {
-
-        this.display = new createjs.Shape().set({
-            x: this.x,
-            y: this.y,
-            mouseEnabled: false,
-            graphics: new createjs.Graphics().beginStroke(this.color).drawCircle(0, 0, 50)
-        });
-        this.stage.addChild(this.display);
-        this.mouseMoveEvent = this.stage.on("stagemousemove", this.drawLine, this);
-        this.mouseUpEvent = this.stage.on("stagemouseup", this.endDraw, this);
-    }
-
-    drawLine(e) {
-        this.display.graphics.clear()
-            .setStrokeStyle(0.6*SQUARE_SIZE)
-            .beginStroke(this.color)
-            .moveTo(0,0)
-            .lineTo(this.stage.mouseX-this.display.x, this.stage.mouseY-this.display.y);
-    }
-
-    endDraw(e) {
-        let element, elements = this.stage.getObjectsUnderPoint(this.stage.mouseX, this.stage.mouseY);
-        let i;
-        
-        for (i = 0; i < elements.length; i++) {
-            if ((elements[i].name == PIN_ID) && (!elements[i].isOccupied)) {
-                element = elements[i];
-                break;               
-            }
-        }
-
-        if (element != null) {
-            this.display.graphics.clear()
-                .setStrokeStyle(0.6*SQUARE_SIZE)
-                .beginStroke(this.color)
-                .moveTo(0,0)
-                .lineTo(element.x-this.display.x + 0.5*SQUARE_SIZE, element.y-this.display.y + 0.5*SQUARE_SIZE);
-            this.pins.push(element.pinObject);
-            this.pins.forEach(pin => {
-                pin.isOccupied = true;
-                pin.occupiedObject = this;
-                console.log(pin);   
-            });
-        } else {
-            this.stage.removeChild(this.display);
-            this.pins = [];
-        }
-
-        this.stage.off("stagemousemove", this.mouseMoveEvent);
-        this.stage.off("stagemouseup", this.mouseUpEvent);
-    }
-}
-
-class Pin {
-    constructor(config) {
-        this.stage = config.stage;
-        this.color = config.color;
-        this.x = config.x;
-        this.y = config.y;
-        this.isOccupied = false;
-        this.occupiedObject = null;
-    }
-
-    render() {
-        const coord = coordToGrid({
-            x: this.x,
-            y: this.y
-        });
-
-        const pin = new createjs.Shape().set({
-            x: coord.x,
-            y: coord.y,
-            cursor: "pointer",
-            name: PIN_ID,
-            pinObject: this
-        });
-
-        const hole = new createjs.Shape().set({
-            x: coord.x + 0.2*SQUARE_SIZE,
-            y: coord.y + 0.2*SQUARE_SIZE
-        });
-
-        drawSquare(this.stage, this.color, pin);
-        drawSquare(this.stage, DARK_GREY, hole, 0.6);
-        pin.on("mousedown", this.handlePress, this); 
-    }
-
-    handlePress(e) {
-        console.log("this");
-        
-        if (!this.isOccupied) {
-            const wire = new Wire({
-                stage: this.stage,
-                color: ORANGE,
-                x: e.target.x + 0.5*SQUARE_SIZE,
-                y: e.target.y + 0.5*SQUARE_SIZE,
-                startingPin: this
-            });
-        }
-    }
-};
-
-class TerminalArray {
-    constructor(config) {
-        this.stage = config.stage;
-        this.color = config.color;
-        this.x = config.x;
-        this.y = config.y;
-        this.width = config.width;
-        this.height = config.height;
-        this.array = null;
-        this.initArray();
-    }
-
-    initArray() {
-        this.array = [];
-        let i, j;
-        for (i = 0; i < this.width; i++) {
-            this.array.push([]);
-            for (j = 0; j < this.height; j++) {
-                this.array[i].push(new Pin({
-                    stage: this.stage,
-                    color: this.color,
-                    x: this.x + i*2,
-                    y: this.y + j*2
-                }));
-            }
-        }
-    }
-
-    render() {
-        let i, j;
-        for (i = 0; i < this.width; i++) {
-            for (j = 0; j < this.height; j++) {
-                this.array[i][j].render(); 
-            }
-        }
-    }
-};
-
-class BusArray {
-    
-    constructor(config) {
-        this.stage = config.stage;
-        this.color = config.color;
-        this.x = config.x;
-        this.y = config.y;
-        this.width = config.width;
-        this.height = config.height;
-        this.array = null;
-        this.initArray();
-    }
-
-    initArray() {
-        this.array = [];
-        let i, j;
-        for (i = 0; i < this.width; i++) {
-            this.array.push([]);
-            for (j = 0; j < this.height; j++) {
-                this.array[i].push(new Pin({
-                    stage: this.stage,
-                    color: this.color,
-                    x: this.x + j*2 + i*BUS_PIN_OFFSET,
-                    y: this.y
-                }));
-            }
-        }
-    }
-
-    render() {
-        let i, j;
-        for (i = 0; i < this.width; i++) {
-            for (j = 0; j < this.height; j++) {
-                this.array[i][j].render(); 
-            }
-        }
-    }
-};
-
-
-class Board {
-    constructor(config) {
-        this.stage = new createjs.Stage("canvas");
-        this.color = config.color;
-        this.size = {
-            w: config.width,
-            h: config.height
-        };
-        this.terminalArray = [];
-        this.busArray = [];
-        this.componentArray = [];
-        this.initBoard();
-        this.initTerminalArray(GREEN);
-        this.initBusArray();
-    }
-
-    initBoard() {
-        createjs.Ticker.addEventListener("tick", this);        
-        this.stage.enableMouseOver();
-        
-        
-    }
-
-    initTerminalArray(color) {
-        let i;
-        for (i = 0; i < 2; i++) {
-            this.terminalArray.push(new TerminalArray({
-                stage: this.stage,
-                color: color,
-                x: TERMINAL_POS.x,
-                y: TERMINAL_POS.y + TERMINAL_OFFSET.y*i,
-                width: TERMINAL_WIDTH,
-                height: TERMINAL_HEIGHT
-            }));
-        }
-    }
-
-    initBusArray() {
-        let i;
-
-        for (i = 0; i < 2; i++) {
-            
-            this.busArray.push(new BusArray({
-                stage: this.stage,
-                color: RED,
-                x: BUS_POS.red.x,
-                y: BUS_POS.red.y + BUS_OFFSETS.red.y*i, 
-                width: BUS_WIDTH,
-                height: BUS_HEIGHT
-            }));
-
-            this.busArray.push(new BusArray({
-                stage: this.stage,
-                color: BLUE,
-                x: BUS_POS.blue.x,
-                y: BUS_POS.blue.y + BUS_OFFSETS.blue.y*i,
-                width: BUS_WIDTH,
-                height: BUS_HEIGHT
-            }));
-        }
-    }
-
-    render() {
-        const boardPos = coordToGrid(BOARD_POS);
-        const boardSize = coordToGrid(this.size);
-        const redStripPos = BUS_STRIPS_POS["red"].map(pos => coordToGrid(pos));
-        const blueStripPos = BUS_STRIPS_POS["blue"].map(pos => coordToGrid(pos));
-        const stripSize = coordToGrid(BUS_STRIPS_SIZE);
-        drawRect(this.stage, boardPos, boardSize, this.color);
-        redStripPos.forEach(pos => {
-            drawRect(this.stage, pos, stripSize, RED);
-        });
-        blueStripPos.forEach(pos => {
-            drawRect(this.stage, pos, stripSize, BLUE);
-        });
-        this.terminalArray.forEach(array => array.render());
-        this.busArray.forEach(array => array.render());
-        this.stage.update();
-    }
-
-    handleEvent(e) {
-        this.stage.update();
-    }
-};
-
-function drawSquare(stage, color, square, sq_scale=1) {
-    square.graphics.beginFill(color)
-            .drawRect(0, 0, SQUARE_SIZE*sq_scale, SQUARE_SIZE*sq_scale);
-    stage.addChild(square);
-}
-
-function drawRect(stage, pos, size, color) {
-    const rect = new createjs.Shape();
-    rect.graphics.beginFill(color).drawRect(pos.x, pos.y, size.w, size.h);
-    stage.addChild(rect);
-    stage.update();
-}
-
-function coordToGrid(values) {
-    Object.keys(values).forEach(key => {
-        values[key] = values[key]*SQUARE_SIZE
-    });
-
-    return values;
+  const webapp = new WebApp();
+  webapp.update();
 }
